c2c8d2db542fdc4b4d21605040ec6bff96adf202
Ashish Anand <ashishanand8824@.com>
2026-03-10 01:19:38 +0530

    fix: add DATABASE_URL to schema datasource

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Running Prisma migrations..."
-DATABASE_URL="$DATABASE_URL" npx prisma migrate deploy --schema=./prisma/schema.prisma --skip-generate
+npx prisma migrate deploy --schema=./prisma/schema.prisma
 echo "Starting server..."
 node dist/server.js
